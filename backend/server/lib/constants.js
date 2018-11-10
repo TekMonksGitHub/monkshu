23dd447a73e92889d998c4d555ef3c8c755dc386
Rohit Kapoor <rvkapoor@tekmonks.com>
2018-11-10 11:07:23 -0500

    Make Monkshu work again.

--- backend/server/lib/constants.js
+++ backend/server/lib/constants.js
@@ -8,8 +8,8 @@ var rootdir = path.resolve(__dirname+"/../");
 
 exports.ROOTDIR = rootdir;
 exports.LIBDIR = rootdir+"/lib";
-exports.LOGSDIR = rootdir+"/../log";
-exports.ACCESSLOG = rootdir+"/../log/admin_api.log.json";
+exports.LOGSDIR = rootdir+"/logs";
+exports.ACCESSLOG = rootdir+"/logs/server.log.ndjson";
 exports.SERVICE_REGISTRY = rootdir + "/conf/service_registry.json";
 exports.TRANSPORT = rootdir + "/conf/transport.json";
 exports.CLUSTERCONF = rootdir+"/conf/cluster.json";
@@ -25,3 +25,8 @@ exports.CRYPT_PASS = "21887als2141rwq294938jwkj3029rqsnj";
 
 /* Key name */
 exports.APIKEY = "org_monkshu_apikey";
+
+/* Constants for the FS Login subsystem */
+exports.SALT_PW = "$2a$10$VFyiln/PpFyZc.ABoi4ppf";
+exports.USERS_DB_PATH = path.resolve(rootdir+"/../db/users");
+exports.USERS_FILE = path.resolve(rootdir+"/../db/users/users.json");
