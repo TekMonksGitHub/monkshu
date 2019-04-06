b72daee139ff54f12045f5bd0740fa7311415d51
Arpit <abs.jain16@gmail.com>
2019-04-06 22:48:04 +0530

    TODO: handle masked data buffer

--- backend/server/websocket/socketService.js
+++ backend/server/websocket/socketService.js
@@ -3,6 +3,11 @@ exports.registerSocket = async socket => {
     socket.on("data", (data) => {
         LOG.info("[INCOMING SOCKET DATA]");
         LOG.info(data);
+
+        // TODO:
+        // Accumulate and unmask the incoming data buffer
+        // Feed the unmasked data to the socket, creating an echo service
+        // Making use of socker.write("...")
     });
 
     socket.on("end", _ => {
