0407f0a8b29519a05b97465cdd1d2a320c10bc5d
Rohit Kapoor <rvkapoor@tekmonks.com>
2019-05-22 15:11:20 -0400

    Allow access control to be specified for backend.

--- backend/server/lib/http_server.js
+++ backend/server/lib/http_server.js
@@ -7,10 +7,12 @@ var http = require("http");
 
 exports.initSync = initSync;
 
-function initSync(port, host="::") {
+function initSync(access_control, port, host="::") {
+	access_control?access_control:"*";
+
 	/* create HTTP server */
 	LOG.info(`Attaching socket listener on ${host}:${port}`);
 	exports.connection = http.createServer((_req, res) => {
-		res.setHeader("Access-Control-Allow-Origin", "*");
+		res.setHeader("Access-Control-Allow-Origin", access_control);
 	}).listen(port, host);
 }
