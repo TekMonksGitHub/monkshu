be30a502d97e9b1757b8c096137c9c5613dd3454
Arpit Jain <abs.jain16@gmail.com>
2019-09-03 12:59:30 +0530

    Restrict reading outside webrooot

--- frontend/server/server.js
+++ frontend/server/server.js
@@ -58,6 +58,10 @@ function handleRequest(req, res) {
 	let pathname = url.parse(req.url).pathname;
 	let fileRequested = `${path.resolve(conf.webroot)}/${pathname}`;
 
+	// don't allow reading outside webroot
+	if (!isSubdirectory(fileRequested, conf.webroot))
+		{sendError(req, res, 404, "Path Not Found."); return;}
+
 	// don't allow reading the server tree, if requested
 	if (conf.restrictServerTree && isSubdirectory(path.dirname(fileRequested), __dirname)) 
 		{sendError(req, res, 404, "Path Not Found."); return;}
