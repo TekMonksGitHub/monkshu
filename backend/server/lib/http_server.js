d315effb53cb2c8953cefd2f676df706d0a2b0c1
Rohit Kapoor <rvkapoor@tekmonks.com>
2019-09-11 13:12:59 -0400

    Cleanup...

--- backend/server/lib/http_server.js
+++ backend/server/lib/http_server.js
@@ -8,11 +8,10 @@ const http = require("http");
 exports.initSync = initSync;
 
 function initSync(access_control, port, host="::", timeout) {
-	access_control?access_control:"*";
-
 	/* create HTTP server */
 	LOG.info(`Attaching socket listener on ${host}:${port}`);
-	let server = http.createServer((_req, res) => {res.setHeader("Access-Control-Allow-Origin", access_control);});
+	let server = http.createServer((_req, res) => 
+		res.setHeader("Access-Control-Allow-Origin", access_control?access_control:"*"));
 	server.timeout = timeout;
 	exports.connection = server.listen(port, host);
 }
--- backend/server/lib/https_server.js
+++ backend/server/lib/https_server.js
@@ -5,21 +5,20 @@
 
 const https = require("https");
 const fs = require("fs");
-
 const sslConf = require(`${CONSTANTS.SSLCONF}`);
-const options = {
-    pfx: fs.readFileSync(sslConf.pfxPath),
-    passphrase: sslConf.pfxPassphrase
-};
 
 exports.initSync = initSync;
 
 function initSync(access_control, port, host = "::", timeout) {
-    access_control?access_control:"*";
+    const options = {
+        pfx: fs.readFileSync(sslConf.pfxPath),
+        passphrase: sslConf.pfxPassphrase
+    };
 
     /* create HTTPS server */
     LOG.info(`Attaching socket listener on ${host}:${port}`);
-    let server = https.createServer(options, (_req, res) => {res.setHeader("Access-Control-Allow-Origin", "*");});
+    let server = https.createServer(options, 
+        (_req, res) => res.setHeader("Access-Control-Allow-Origin", access_control?access_control:"*"));
     server.timeout = timeout;
 	exports.connection = server.listen(port, host);
 };
