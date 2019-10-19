fd194c9becd6a962de50a24655685d9c79c3e9ab
Arpit Jain <abs.jain16@gmail.com>
2019-10-20 02:32:50 +0530

    Fix for HTTPS server and options

--- backend/server/lib/http_server.js
+++ backend/server/lib/http_server.js
@@ -4,17 +4,19 @@
  */
 
 const http = require("http");
+const https = require("https");
+const fs = require("fs");
 const conf = require(`${CONSTANTS.HTTPDCONF}`);
 
 exports.initSync = initSync;
 
-function initSync(port, host="::") {
-	let options = conf.ssl ? {pfx: fs.readFileSync(conf.pfxPath), passphrase: conf.pfxPassphrase} : null;
-	
+function initSync(port, host = "::") {
+	const options = conf.ssl ? { pfx: fs.readFileSync(conf.pfxPath), passphrase: conf.pfxPassphrase } : null;
+
 	/* create HTTP/S server */
 	LOG.info(`Attaching socket listener on ${host}:${port}`);
-	let listener = (_req, res) => Object.keys(conf.headers).forEach(header => res.setHeader(header, conf.headers[header]));
-	let server = options ? http.createServer(options, listener) : http.createServer(listener);
+	const listener = (_req, res) => Object.keys(conf.headers).forEach(header => res.setHeader(header, conf.headers[header]));
+	const server = options ? https.createServer(options, listener) : http.createServer(listener);
 	server.timeout = conf.timeout;
 	exports.connection = server.listen(port, host);
 }
