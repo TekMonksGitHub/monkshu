dcb05b4a1b7bc2377076e612b670bfddd4ce20a0
Rohit Kapoor <rvkapoor@tekmonks.com>
2020-03-02 21:41:26 -0500

    Minor fixes to the API encryption extension.

--- backend/server/lib/apiregistry_extensions/encryptedbodydecoderencoder.js
+++ backend/server/lib/apiregistry_extensions/encryptedbodydecoderencoder.js
@@ -4,18 +4,24 @@
  * 
  * Encrypted data decoder
  */
+const crypt = require(CONSTANTS.LIBDIR+"/crypt.js");
+const utils = require(CONSTANTS.LIBDIR+"/utils.js");
 
-function decodeIncomingData(apiregentry, _url, data, _headers) {
+function decodeIncomingData(apiregentry, _url, data, headers) {
     if (!apiregentry.query.encrypted || !apiregentry.query.encrypted.toLowerCase() === "true") return data;  // not encrypted
 
     return utils.getObjectKeyValueCaseInsensitive(headers,"Content-Type").toLowerCase() == "application/json" ?
         crypt.decrypt(JSON.parse(data).data) : crypt.decrypt(data);
 }
 
-function encodeResponse(apiregentry, _url, respObj, _reqHeaders, _respHeaders) {
+function encodeResponse(apiregentry, _url, respObj, reqHeaders, _respHeaders) {
     if (!apiregentry.query.encrypted || !apiregentry.query.encrypted.toLowerCase() === "true") return respObj;  // not encrypted
 
-    return {data: crypt.encrypt(respObj)};
+    const acceptedEncodings = (utils.getObjectKeyValueCaseInsensitive(reqHeaders,"Accept") || "").toLowerCase();
+    const sendJSONBack = acceptedEncodings != "application/json" && acceptedEncodings != "application/*" && 
+        acceptedEncodings != "*/*" || apiregentry.query.notRESTAPI ? false : true;
+
+    return sendJSONBack ? JSON.stringify({data: crypt.encrypt(respObj)}) : crypt.encrypt(respObj);
 }
 
 module.exports = {decodeIncomingData, encodeResponse}
