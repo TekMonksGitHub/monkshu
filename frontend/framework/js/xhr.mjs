fe604141cdccf04e32e995fc289b633b1c292328
Rohit Kapoor <rvkapoor@tekmonks.com>
2020-10-20 11:45:24 -0400

    Fix XHR for JSON payloads.

--- frontend/framework/js/xhr.mjs
+++ frontend/framework/js/xhr.mjs
@@ -17,7 +17,7 @@ async function rest(url, type, params) {
         catch (e) {return {result:false, reason:e}}
     }
     else {
-        try {return JSON.parse(await post(url, JSON.stringify(params)));}
+        try {return JSON.parse(await post(url, JSON.stringify(params),"application/json"));}
         catch (e) {return {result:false, reason:e}}
     }
 }
@@ -27,18 +27,18 @@ function get(url) {
         callXHR("GET", url).then(response => resolve(response)).catch(err => reject(err)));
 }
 
-function post(url, params) {
+function post(url, params, contentType) {
     return new Promise((resolve, reject) => 
-        callXHR("POST", url, params).then(response => resolve(response)).catch(err => reject(err)));
+        callXHR("POST", url, params, contentType).then(response => resolve(response)).catch(err => reject(err)));
 }
 
 export const xhr = {rest, get, post};
 
-function callXHR(method, url, params) {
+function callXHR(method, url, params, contentType="application/x-www-form-urlencoded") {
     return new Promise((resolve, reject) => {
         let xhr = new XMLHttpRequest();
         xhr.open(method, url, true);
-        if (method.toUpperCase() == "POST" ) xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+        if (method.toUpperCase() == "POST" ) xhr.setRequestHeader("Content-type",contentType);
         xhr.followRedirects = true;
 
         xhr.onload = _ => {
