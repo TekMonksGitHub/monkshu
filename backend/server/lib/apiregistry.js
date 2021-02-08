0df24787f519d7bc341fe661ee4103fb29b13cf1
Arpit Jain <abs.jain16@gmail.com>
2021-02-08 18:09:54 +0530

    Fix for jwttokenmanager to use clustermemory (#24)
    * Fix for jwttokenmanager to use clustermemory
    
    * Nomenclature fixes

--- backend/server/lib/apiregistry.js
+++ backend/server/lib/apiregistry.js
@@ -9,7 +9,7 @@ const fs = require("fs");
 const path = require("path");
 const urlMod = require("url");
 const app = require(`${CONSTANTS.LIBDIR}/app.js`);
-const API_REG_DISTM_KEY = "__org_monkshu_apiregistry";
+const API_REG_DISTM_KEY = "__org_monkshu_apiregistry_key";
 let decoders, encoders, headermanagers, securitycheckers;
 
 function initSync() {
