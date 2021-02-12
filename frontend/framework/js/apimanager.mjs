029af7fb94abb02bfc10aa94d876fd5303a3d049
Rohit Kapoor <rvkapoor@tekmonks.com>
2021-02-12 11:02:48 -0500

    Enable frontend API manager to call binary APIs.

--- frontend/framework/js/apimanager.mjs
+++ frontend/framework/js/apimanager.mjs
@@ -30,35 +30,41 @@ const APIMANAGER_SESSIONKEY = "__org_monkshu_APIManager";
  * @return {Object} Javascript result object or null on error
  */
 async function rest(url, type, req, sendToken, extractToken, canUseCache) {
-    type = type || "GET"; const urlHost = new URL(url).host; 
-    const storage = _getAPIManagerStorage();
-
-    const apiResponseCacheKey = url.toString()+type+JSON.stringify(req)+sendToken+extractToken;
+    const storage = _getAPIManagerStorage(), apiResponseCacheKey = url.toString()+type+JSON.stringify(req)+sendToken+extractToken;
     if (canUseCache && storage.apiResponseCache[apiResponseCacheKey]) return storage.apiResponseCache[apiResponseCacheKey]; // send cached response if acceptable and available
 
-    let jsonReq;
-    if (type.toUpperCase() == "GET" && req) {
-        for (const key of Object.keys(req)) jsonReq = jsonReq ? jsonReq+`&${_getKeyValAsURLParam(key, req[key])}`:`${_getKeyValAsURLParam(key, req[key])}`;
-        if (jsonReq) {url += `?${jsonReq}`; jsonReq = null;}
-    } else jsonReq = typeof (req) == "object" ? JSON.stringify(req) : req;
-
-    const headers = {"Accept":"application/json"}; if (type.toUpperCase() != "GET" && type.toUpperCase() != "DELETE") headers["Content-type"] = "application/json";
-    if (sendToken) headers.Authorization = `Bearer ${storage.tokenManager[`${urlHost}_${sendToken.type?sendToken.type:"access"}`]}`;
-    if (storage.keys[url] || storage.keys["*"]) 
-        headers[storage.keyHeader] = storage.keys[url] ? storage.keys[url] : storage.keys["*"];
-
-    const fetchInit = { method: type, credentials: "omit",    // we will send token if needed
-        headers, redirect: "follow", referrerPolicy: "origin"};
-    if (jsonReq) fetchInit.body =  jsonReq;
-    const response = await fetch(url, fetchInit);
+    const {fetchInit, url: urlToCall} = _createFetchInit(url, type, req, sendToken), response = await fetch(urlToCall, fetchInit);
     if (response.ok) {
         const respObj = await response.json();
-        const headersBack = {}; for (const headerBack of response.headers) headersBack[headerBack[0]] = headerBack[1];
-        if (extractToken && respObj.access_token) _extractAddToken(respObj.access_token, urlHost);
-        else if (extractToken && headersBack.access_token) _extractAddToken(headersBack.access_token, urlHost);
-        
-        if (canUseCache) storage.apiResponseCache[apiResponseCacheKey] = respObj; return respObj;   // cache response if allowed and return
+        if (extractToken) _extractTokens(response, respObj);
+        if (canUseCache) storage.apiResponseCache[apiResponseCacheKey] = respObj; // cache response if allowed
+        return respObj;   
     } else return null;
+}
+
+/**
+ * Makes a BLOB API call, i.e. download a binary object.
+ * 
+ * @param {string} url The URL to call
+ * @param {string} filename The downloaded file's name
+ * @param {string} type  Can we GET, POST, DELETE etc.
+ * @param {object} req Javascript/JSON object to send
+ * @param {object|boolean} sendToken Optional: {type: "access" or other types}, if set to true then access is assumed
+ * @param {boolean} extractToken Optional: true or false to extract incoming tokens as a result of the API call
+ * 
+ * @return {Object} Downloads the BLOB
+ */
+async function blob(url, filename, type, req, sendToken, extractToken) {
+    const {fetchInit, url: urlToCall} = _createFetchInit(url, type, req, sendToken, "*/*");
+    const response = await fetch(urlToCall, fetchInit);
+    if (response.ok) {
+        if (extractToken) _extractTokens(response);
+        const respBlob = await response.blob();
+        const url = window.URL.createObjectURL(respBlob), link = document.createElement("a");
+        link.href = url; link.download = filename;
+        document.body.appendChild(link); link.click(); link.remove();  
+        return true;
+    } else return false;
 }
 
 /**
@@ -69,6 +75,32 @@ async function rest(url, type, req, sendToken, extractToken, canUseCache) {
 function registerAPIKeys(apikeys, apiKeyHeaderName) {
     _modifyAPIManagerStorage("keys", apikeys);
     if (apiKeyHeaderName) _modifyAPIManagerStorage("keyHeader", apiKeyHeaderName);
+}
+
+function _createFetchInit(url, type, req, sendToken, acceptHeader) {
+    type = type || "GET"; const urlHost = new URL(url).host; 
+    const storage = _getAPIManagerStorage();
+
+    let jsonReq;
+    if (type.toUpperCase() == "GET" && req) {
+        for (const key of Object.keys(req)) jsonReq = jsonReq ? jsonReq+`&${_getKeyValAsURLParam(key, req[key])}`:_getKeyValAsURLParam(key, req[key]);
+        if (jsonReq) {url += `?${jsonReq}`; jsonReq = null;}
+    } else jsonReq = req && typeof (req) == "object" ? JSON.stringify(req) : req;
+
+    const headers = {"Accept":acceptHeader||"application/json"}; 
+    if (type.toUpperCase() != "GET" && type.toUpperCase() != "DELETE") headers["Content-type"] = "application/json";
+    if (sendToken) headers.Authorization = `Bearer ${storage.tokenManager[`${urlHost}_${sendToken.type?sendToken.type:"access"}`]}`;
+    if (storage.keys[url] || storage.keys["*"]) headers[storage.keyHeader] = storage.keys[url] ? storage.keys[url] : storage.keys["*"];
+
+    const fetchInit = {method: type, credentials: "omit", headers, redirect: "follow", referrerPolicy: "origin"};
+    if (jsonReq) fetchInit.body =  jsonReq; return {fetchInit, url};
+}
+
+function _extractTokens(response, jsonResponseObj) {
+    const urlHost = new URL(response.url).host, headersBack = {}; 
+    for (const headerBack of response.headers) headersBack[headerBack[0]] = headerBack[1];
+    if (jsonResponseObj && jsonResponseObj.access_token) _extractAddToken(jsonResponseObj.access_token, urlHost);
+    else if (headersBack.access_token) _extractAddToken(headersBack.access_token, urlHost);
 }
 
 function _getKeyValAsURLParam(key, val) {
@@ -110,4 +142,4 @@ function _modifyAPIManagerStorage(key, value) {
     storage[key] = value;
 }
 
-export const apimanager = {rest, registerAPIKeys};
+export const apimanager = {rest, blob, registerAPIKeys};
