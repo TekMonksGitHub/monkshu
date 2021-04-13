7b10a2d928adaaa6dce3c771dfcef04fe0459024
Rohit Kapoor <rvkapoor@tekmonks.com>
2021-04-13 01:38:15 -0400

    Minor cleanup.

--- frontend/server/lib/httpClient.js
+++ frontend/server/lib/httpClient.js
@@ -2,9 +2,7 @@
  * (C) 2021 TekMonks. All rights reserved. 
  * 
  */
-
-const path = require("path");
-const monkshu_root = path.resolve(__dirname + "/../../../");
+const monkshu_root = require("path").resolve(__dirname + "/../../../");
 const httpClientReal = require(`${monkshu_root}/backend/server/lib/httpClient.js`);
 
 module.exports = {...httpClientReal};
