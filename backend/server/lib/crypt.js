28ee1e048cd583f17bc555d1aee3e7d41bf9511f
Rohit Kapoor <rvkapoor@tekmonks.com>
2021-04-20 04:38:33 -0400

    Fix crypt.js

--- backend/server/lib/crypt.js
+++ backend/server/lib/crypt.js
@@ -9,7 +9,7 @@ const crypt = require(CONSTANTS.CRYPTCONF);
 function encrypt(text, key = crypt.key) {
 	const iv = Buffer.from(cryptmod.randomBytes(16)).toString("hex").slice(0, 16);
 	const password_hash = cryptmod.createHash("md5").update(key, "utf-8").digest("hex").toUpperCase();
-	const cipher = cryptmod.createCipheriv(crypt.crypt_algo, password_hash, iv);
+	const cipher = cryptmod.createCipheriv(crypt.crypt_algo||"aes-256-ctr", password_hash, iv);
 	let crypted = cipher.update(text, "utf8", "hex");
 	crypted += cipher.final("hex");
 	return crypted + iv;
@@ -19,11 +19,13 @@ function decrypt(text, key = crypt.key) {
 	const iv = text.slice(text.length - 16, text.length);
 	text = text.substring(0, text.length - 16);
 	const password_hash = cryptmod.createHash("md5").update(key, "utf-8").digest("hex").toUpperCase();
-	const decipher = cryptmod.createDecipheriv(crypt.crypt_algo, password_hash, iv);
+	const decipher = cryptmod.createDecipheriv(crypt.crypt_algo||"aes-256-ctr", password_hash, iv);
 	let decrypted = decipher.update(text, "hex", "utf8");
 	decrypted += decipher.final("utf8");
 	return decrypted;
 }
+
+module.exports = { encrypt, decrypt }
 
 if (require.main === module) main();
 function main() {
@@ -36,5 +38,3 @@ function main() {
 
 	console.log(module.exports[args[0]](args[1]));
 }
-
-module.exports = { encrypt, decrypt, main };
