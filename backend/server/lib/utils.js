1fa293d1b5237b797ad77271ff578cfc3762753b
TekMonksGitHub <rvkapoor@tekmonks.com>
2021-05-26 01:49:34 +0900

    Fix to utils in backend server. Need OS module.

--- backend/server/lib/utils.js
+++ backend/server/lib/utils.js
@@ -4,6 +4,7 @@
  */
 
 const fs = require("fs");
+const os = require("os");
 const path = require("path");
 const mkdirAsync = require("util").promisify(fs.mkdir);
 const lstatAsync = require("util").promisify(fs.lstat);
