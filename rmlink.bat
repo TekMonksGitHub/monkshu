2ed959f8cb8fd682329fcec997afbfbebbb2187c
TekMonksGitHub <rvkapoor@tekmonks.com>
2021-06-09 15:40:56 +0900

    Add in modified app scripts

--- rmlink.bat
+++ rmlink.bat
@@ -1,15 +1,6 @@
 @echo off
 
-set APP_NAME=%1
-if not defined APP_NAME GOTO USAGE
-
-rmdir .\backend\apps\%APP_NAME%
-rmdir .\frontend\apps\%APP_NAME%
+rmdir .\backend\apps
+rmdir .\frontend\apps
 
 echo Done.
-goto END
-
-:USAGE
-echo Usage: rmlink.bat [name of the application]
-
-:END
--- rmlink.sh
+++ rmlink.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-
-if [ "$1" == "" ]; then
-	echo Usage: $0 [name of the application]
-	exit 1
-fi
-APP_NAME=$1
 CURPATH="$( cd "$( dirname "$0" )" && pwd )"
 
-rm $CURPATH/backend/apps/$APP_NAME
-rm $CURPATH/frontend/apps/$APP_NAME
+rm $CURPATH/backend/apps
+rm $CURPATH/frontend/apps
 
 echo Done.
