56f080ab085ace0d599caa617cd1eff742ddf852
TekMonksGitHub <rvkapoor@tekmonks.com>
2021-07-08 00:53:20 +0900

    Add discoverable listener support for blackboard.

--- frontend/framework/js/blackboard.mjs
+++ frontend/framework/js/blackboard.mjs
@@ -4,14 +4,27 @@
  * License: See enclosed LICENSE file.
  */
 
-const topics = {};
+const topics = {}, DISCOVERABLE_FLAG = "__org_monkshu_blackboard_discoverable_listener"
 
-function registerListener(topic, listener) {
+/**
+ * Registers listeners for messages
+ * @param topic The topic to listen to, usually a string
+ * @param listener The listener which will be passed the messages
+ * @param discoverable Whether the listener can be discovered from the blackboard, 
+ *                     by default the blackboard doesn't let callers know the listeners
+ */
+function registerListener(topic, listener, discoverable) {
     topic = topic.toLowerCase();
     if (!topics[topic]) topics[topic] = []; 
+    if (discoverable) listener[DISCOVERABLE_FLAG] = true;
     topics[topic].push(listener);
 }
 
+/**
+ * Broadcasts the given message asynchornously. No replies are handled.
+ * @param topic The topic of the message, usually a string
+ * @param message The message, can be a string, JSON string or any JS object
+ */
 function broadcastMessage(topic, message) {
     topic = topic.toLowerCase();
     if (!topics[topic]) return; // no such topic exists / no one interested
@@ -19,4 +32,18 @@ function broadcastMessage(topic, message) {
     for (const listener of topics[topic]) listener(message);
 }
 
-export const blackboard = {registerListener, broadcastMessage};
+/**
+ * Returns the array of discoverable listenrs for the given topic
+ * @param topic The topic for which discoverable listeners are required
+ * @returns The array of discoverable listenrs for the given topic
+ */
+function getListeners(topic) {
+    topic = topic.toLowerCase();
+    if (!topics[topic]) return []; // no such topic exists / no one interested
+    
+    const discoverableListners = [];
+    for (const listener of topics[topic]) if (listener[DISCOVERABLE_FLAG]) discoverableListners.push(listener);
+    return discoverableListners;
+}
+
+export const blackboard = {registerListener, broadcastMessage, getListeners};
