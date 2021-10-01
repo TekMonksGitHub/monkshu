809e6e3d041d9716534755b7a8a3a8e0e5643251
TekMonksGitHub <rvkapoor@tekmonks.com>
2021-10-01 17:42:14 +0900

    Updated cacheworker and debug flag for log.

--- frontend/framework/js/cacheworker.mjs
+++ frontend/framework/js/cacheworker.mjs
@@ -1,8 +1,11 @@
 /**
  * Service worker to support offline webapps.
+ * This service worker is specifically coded for Monkshu, in that
+ * it strips out URL queries and parameters for caching. 
  * (C) TekMonks Corp. 
  * License: See enclosed LICENSE file.
  */
+import {LOG} from "./log.mjs";
 import {MONKSHU_CONSTANTS} from "/framework/js/constants.mjs";
 
 /**
@@ -13,7 +16,11 @@ import {MONKSHU_CONSTANTS} from "/framework/js/constants.mjs";
 function enableCacheWorker() {
     const cacheFunction = async (appName, listOfFilesToCache) => {
         const cache = await caches.open("__org_monkshu__app_cache"+appName);
-        return cache.addAll(listOfFilesToCache);
+        for (const file of listOfFilesToCache) {
+            const response = await fetch(file); await cache.put(file, response); 
+            if (!response.ok) LOG.error(`Can't cache ${file}, response not ok, response status is ${response.status}, ${response.statusText}.`);
+        }
+        LOG.info("Service worker caching completed.");
     }
 
     self.addEventListener("install", e => {
@@ -24,9 +31,9 @@ function enableCacheWorker() {
         e.waitUntil(clients.claim())    // we will handle everything
     });   
 
-    self.addEventListener("message", e => {
+    self.addEventListener("message", async e => {
         const message = e.data; if (message.id != MONKSHU_CONSTANTS.CACHEWORKER_MSG) return;    // not for us        
-        if (message.op == "cache") e.waitUntil(cacheFunction(message.appName, message.listOfFilesToCache));
+        if (message.op == "cache") await cacheFunction(message.appName, message.listOfFilesToCache);
     })
 }
 
@@ -35,8 +42,9 @@ function enableCacheWorker() {
  */
 function registerHandleRequests() {
     const requestHandler = async e => {
-        const justURL = url => url.split("?")[0].split("#")[0];
+        const justURL = url => url.replace(/([^:]\/)\/+/g, "$1").split("?")[0].split("#")[0];
         const response = await caches.match(justURL(e.request.url));    // monkshu assets don't change based on query params
+        if (!response) LOG.debug(`Cache miss for ${e.request.url}`);
         return response || fetch(e.request);    // we won't add to cache, APIs are dynamic for example
     }
 
--- frontend/framework/js/log.mjs
+++ frontend/framework/js/log.mjs
@@ -1,11 +1,15 @@
-/* 
+/**
+ * Logger.
  * (C) 2015 TekMonks. All rights reserved.
  * License: See enclosed LICENSE file.
  */
+
+let debugFlag = true;
 
 const debug = s => console.debug(`[DEBUG] ${s}`);
 const info = s => console.log(`[INFO] ${s}`);
 const error = s => console.error(`[ERROR] ${s}`);
 const warn = s => console.warn(`[WARNING] ${s}`);
+const setDebug = flag => debugFlag = flag;
 
-export const LOG = {debug, info, error, warn};
+export const LOG = {debug, info, error, warn, setDebug};
