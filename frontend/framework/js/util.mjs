5ba2467c21e184de1034ce870edbc5f6a70ff6fb
TekMonksGitHub <rvkapoor@tekmonks.com>
2022-05-05 15:46:14 +0800

    Add in UUID generation into core utils.

--- frontend/framework/js/util.mjs
+++ frontend/framework/js/util.mjs
@@ -182,11 +182,26 @@ function getChildByID(element, id) {
  * @param {number} interval The interval in milliseconds
  * @return The timer
  */
- const setIntervalImmediately = (functionToCall, interval) => {functionToCall(); return setInterval(functionToCall, interval)};
+const setIntervalImmediately = (functionToCall, interval) => {functionToCall(); return setInterval(functionToCall, interval)};
 
 /** @return Fully resolved URL for given relative URL */
 const resolveURL = urlOrPartialPath => new URL(urlOrPartialPath, window.location.href).href;
 
+/** @return Generated UUID */
+function generateUUID() { // Public Domain/MIT: from https://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid
+    let d = new Date().getTime();//Timestamp
+    let d2 = ((typeof performance !== 'undefined') && performance.now && (performance.now()*1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        let r = Math.random() * 16;//random number between 0 and 16
+        if(d > 0){//Use timestamp until depleted
+            r = (d + r)%16 | 0; d = Math.floor(d/16);
+        } else {//Use microseconds since page-load if supported
+            r = (d2 + r)%16 | 0; d2 = Math.floor(d2/16);
+        }
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+}
+
 export const util = {getCSSRule, getFunctionFromString, replaceURLParamValue, parseBoolean, escapeHTML, getModulePath,
     downloadFile, uploadAFile, getFileData, clone, resolveURL, safeURIDecode, getChildByID, getChildrenByTagName,
-    setIntervalImmediately}
+    setIntervalImmediately, generateUUID}
