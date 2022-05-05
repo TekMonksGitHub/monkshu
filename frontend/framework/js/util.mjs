578ae5dd2ba1701621a673a367d468996a8db4d8
TekMonksGitHub <rvkapoor@tekmonks.com>
2022-05-05 15:47:17 +0800

    Minor cleanup.

--- frontend/framework/js/util.mjs
+++ frontend/framework/js/util.mjs
@@ -190,8 +190,8 @@ const resolveURL = urlOrPartialPath => new URL(urlOrPartialPath, window.location
 /** @return Generated UUID */
 function generateUUID() { // Public Domain/MIT: from https://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid
     let d = new Date().getTime();//Timestamp
-    let d2 = ((typeof performance !== 'undefined') && performance.now && (performance.now()*1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    let d2 = ((typeof performance !== "undefined") && performance.now && (performance.now()*1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
         let r = Math.random() * 16;//random number between 0 and 16
         if(d > 0){//Use timestamp until depleted
             r = (d + r)%16 | 0; d = Math.floor(d/16);
