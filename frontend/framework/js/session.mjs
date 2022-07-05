074dedb556411e7bb5b1504a6e8acd8ad293d5d9
TekMonksGitHub <rvkapoor@tekmonks.com>
2022-07-05 15:46:12 +0530

    Fix sessions to return same object unless refresh

--- frontend/framework/js/session.mjs
+++ frontend/framework/js/session.mjs
@@ -1,4 +1,4 @@
-/* 
+/** 
  * (C) 2018 TekMonks. All rights reserved.
  * License: See enclosed LICENSE file.
  */
@@ -7,12 +7,17 @@
 // Cookieless HTML5 Session Support
 ////////////////////////////////////////////////    
 
+const _internalHash = {};   // within the same browser tab (unless refreshed) makes sure get returns same objects.
+
 const set = (key, item) => {
     const value = JSON.stringify(item);
-    if (key && item) sessionStorage.setItem(key, value);
+    if (key && item) {_internalHash[key] = item; sessionStorage.setItem(key, value);}
 }
-const get = key => sessionStorage.getItem(key) ? _getProxy(JSON.parse(sessionStorage.getItem(key)),key) : undefined;
-const remove = key => sessionStorage.removeItem(key);
+const get = key => {
+    if ((!_internalHash[key]) && sessionStorage.getItem(key)) _internalHash[key] = JSON.parse(sessionStorage.getItem(key));
+    return _internalHash[key]?_getProxy(_internalHash[key]):undefined;
+}
+const remove = key => {delete _internalHash[key]; sessionStorage.removeItem(key);}
 const destroy = _ => sessionStorage.clear();
 const contains = key => get(key)?true:false;
 const keys = _ => Object.keys(sessionStorage);
@@ -35,6 +40,7 @@ function _getProxy(object, key) {
 
         get(target, name) { // intercept for NativeWrappers to convert them to JSON
             if (name == "toJSON") {return target.toJson} 
+            if (name == "_______org_monkshu_clientwebsession_wrapped_object_______") return object;
             else if (typeof target[name] === 'object' && target[name] !== null) return new Proxy(target[name], handler);
             else return target[name];
         }
