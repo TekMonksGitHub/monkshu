a9cfd2171521533b7e20920b97135e453ca35af1
TekMonksGitHub <rvkapoor@tekmonks.com>
2022-10-19 18:02:35 +0900

    Make crypt.js type safe.

--- backend/server/lib/crypt.js
+++ backend/server/lib/crypt.js
@@ -6,21 +6,35 @@ if (!global.CONSTANTS) global.CONSTANTS = require(__dirname + "/constants.js");	
 const cryptmod = require("crypto");
 const crypt = require(CONSTANTS.CRYPTCONF);
 
+/**
+ * Encrypts the given string
+ * @param {String or Buffer} text Text to encrypt, .toString is called if it is not one of these to convert (dangerous).
+ * @param {String} key The encryption key to use, the key in conf/crypt.json is used if this is skipped
+ * @returns The encrypted text as string in UTF8 encoding
+ */
 function encrypt(text, key = crypt.key) {
 	const iv = Buffer.from(cryptmod.randomBytes(16)).toString("hex").slice(0, 16);
 	const password_hash = cryptmod.createHash("md5").update(key, "utf-8").digest("hex").toUpperCase();
 	const cipher = cryptmod.createCipheriv(crypt.crypt_algo||"aes-256-ctr", password_hash, iv);
-	let crypted = cipher.update(text, "utf8", "hex");
+	const textToEncrypt = ((typeof text !== "string") && (!Buffer.isBuffer(text))) ? text.toString() : text;
+	let crypted = cipher.update(textToEncrypt, "utf8", "hex");
 	crypted += cipher.final("hex");
 	return crypted + iv;
 }
 
+/**
+ * Decrypts the given string
+ * @param {String or Buffer} text Text to decrypt, .toString is called if it is not one of these to convert (dangerous).
+ * @param {String} key The encryption key to use, the key in conf/crypt.json is used if this is skipped
+ * @returns The decrypted text as string in UTF8 encoding
+ */
 function decrypt(text, key = crypt.key) {
 	const iv = text.slice(text.length - 16, text.length);
 	text = text.substring(0, text.length - 16);
 	const password_hash = cryptmod.createHash("md5").update(key, "utf-8").digest("hex").toUpperCase();
 	const decipher = cryptmod.createDecipheriv(crypt.crypt_algo||"aes-256-ctr", password_hash, iv);
-	let decrypted = decipher.update(text, "hex", "utf8");
+	const textToDecrypt = ((typeof text !== "string") && (!Buffer.isBuffer(text))) ? text.toString() : text;
+	let decrypted = decipher.update(textToDecrypt, "hex", "utf8");
 	decrypted += decipher.final("utf8");
 	return decrypted;
 }
