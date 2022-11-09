2c2e91dab803be5e7a198b819462949830814351
TekMonksGitHub <rvkapoor@tekmonks.com>
2022-11-09 17:04:17 +0800

    Add time to log messages for console.

--- frontend/framework/js/log.mjs
+++ frontend/framework/js/log.mjs
@@ -7,14 +7,15 @@ import {apimanager as apiman} from "/framework/js/apimanager.mjs";
 
 let debugFlag = true, remoteFlag = false, remoteAPI;
 
-const debug = s => {if (debugFlag) console.debug(`[DEBUG] ${s}`); if (remoteFlag) _logremote("debug", s);}
-const info = s => {console.log(`[INFO] ${s}`); if (remoteFlag) _logremote("info", s);}
-const error = s => {console.error(`[ERROR] ${s}`); if (remoteFlag) _logremote("error", s);}
-const warn = s => {console.warn(`[WARNING] ${s}`); if (remoteFlag) _logremote("warn", s);}
+const debug = s => {if (debugFlag) console.debug(_getLogLine("DEBUG", s)); if (remoteFlag) _logremote("debug", s);}
+const info = s => {console.log(_getLogLine("INFO", s)); if (remoteFlag) _logremote("info", s);}
+const error = s => {console.error(_getLogLine("ERROR", s)); if (remoteFlag) _logremote("error", s);}
+const warn = s => {console.warn(_getLogLine("WARNING", s)); if (remoteFlag) _logremote("warn", s);}
 const setDebug = flag => debugFlag = flag;
 const setRemote = (flag, api) => { remoteFlag = flag; if (flag) remoteAPI = api; }
 
 const _logremote = (level, message) => { try {apiman.rest(remoteAPI, "POST", {level, message}); } catch (err) { console.log("Remote log error"+err);} }
+const _getLogLine = (level, message) => {const now = new Date(); return `[${level}] [${now.toLocaleString()}] ${message}`;}
 
 export const log = {debug, info, error, warn, setDebug, setRemote};
 export const LOG = log; // for backwards compatibility
