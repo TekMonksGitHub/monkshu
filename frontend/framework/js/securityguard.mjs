f8fa0748c09ce83fed4abc0a78bc1b233ea90c3d
TekMonksGitHub <rvkapoor@tekmonks.com>
2023-04-03 03:17:39 +0530

    Add wildcards for security guard permission paths.

--- frontend/framework/js/securityguard.mjs
+++ frontend/framework/js/securityguard.mjs
@@ -11,8 +11,9 @@ let _current_role  = session.get("_com_monkshu_securityguard_current_role");
 
 function isAllowed(resource, role=_current_role) {
     if (_app_interceptor) return _app_interceptor.isAllowed(resource, role);
-    else if (!role) return false;
-    else return _PERMISSIONS_MAP[role]?.includes(resource) ||  _PERMISSIONS_MAP[role] == "*";
+
+    else if ((!role) || (!_PERMISSIONS_MAP[role])) return false;
+    else return _PERMISSIONS_MAP[role].some(path => _doesResourceMatchPermissionPath(resource, path));
 }
 
 function setAppInterceptor(interceptor) {
@@ -36,6 +37,13 @@ function getCurrentRole() {return _current_role}
 function addPermission(resource, role) {
     if (_PERMISSIONS_MAP[role] && !_PERMISSIONS_MAP[role].includes(resource)) _PERMISSIONS_MAP[role].push(resource);
     session.set("_com_monkshu_securityguard_permissions_map", _PERMISSIONS_MAP);
+}
+
+function _doesResourceMatchPermissionPath(resource, permissionpath) {
+    if (!permissionpath.includes("*")) return resource == permissionpath;   // only exact match unless * wildcard is present in the permission
+    const _shellToJSRegexp =  shellRegex => shellRegex.replace(/[.+^${}()/|[\]\\]/g, '\\$&').replace(/\*/g, '.*').replace(/\?/g, '.');
+    const jsRegExp = _shellToJSRegexp(permissionpath), regExpObj = new RegExp(jsRegExp);  
+    return resource.match(regExpObj) ? true : false;
 }
 
 export const securityguard = {isAllowed, setAppInterceptor, getAppInterceptor, setPermissionsMap, getPermissionsMap, 
