90379fd93a75077d2a3e7269c1aac8f3acf14901
TekMonksGitHub <rvkapoor@tekmonks.com>
2023-04-27 02:27:02 +0530

    Add UUID generator to the backend utils.

--- backend/server/lib/utils.js
+++ backend/server/lib/utils.js
@@ -398,7 +398,23 @@ function _getObjectPathSplits(path) {
     return final;
 }
 
+/** @return Generated UUID */
+function generateUUID() { // Public Domain/MIT: from https://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid
+    const {performance} = require("perf_hooks");
+    let d = new Date().getTime();//Timestamp
+    let d2 = ((typeof performance !== "undefined") && performance.now && (performance.now()*1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+        let r = Math.random() * 16;//random number between 0 and 16
+        if(d > 0){//Use timestamp until depleted
+            r = (d + r)%16 | 0; d = Math.floor(d/16);
+        } else {//Use microseconds since page-load if supported
+            r = (d2 + r)%16 | 0; d2 = Math.floor(d2/16);
+        }
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+}
+
 module.exports = { parseBoolean, getDateTime, queryToObject, escapedSplit, getTimeStamp, getUnixEpoch, 
     getObjectKeyValueCaseInsensitive, getObjectKeyNameCaseInsensitive, getTempFile, copyFileOrFolder, getClientIP, 
     getServerHost, getClientPort, getEmbeddedIPV4, setIntervalImmediately, expandIPv6Address, analyzeIPAddr, 
-    watchFile, clone, walkFolder, rmrf, getObjProperty, setObjProperty, requireWithDebug };
+    watchFile, clone, walkFolder, rmrf, getObjProperty, setObjProperty, requireWithDebug, generateUUID };
