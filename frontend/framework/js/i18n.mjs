c9187d84d631256ec00423243601e4c6d876e482
TekMonksGitHub <rvkapoor@tekmonks.com>
2023-06-04 02:43:11 +0530

    Fixes for pluggabler i18n bundles to load properly

--- frontend/framework/js/i18n.mjs
+++ frontend/framework/js/i18n.mjs
@@ -5,14 +5,18 @@
 import {router} from "/framework/js/router.mjs";
 import {session} from "/framework/js/session.mjs";
 
-let i18nCached = {};
-const appPaths = [];
+const APP_PATHS_SESSION_KEY = "__org_monkshu_i18n_app_paths";
 
-const init = (appsPathIn) => appPaths.push(...(Array.isArray(appsPathIn)?appsPathIn:[appsPathIn]));
+let i18nCached = {};
+
+const init = appPathsIn => {
+	for (const appPath of Array.isArray(appPathsIn)?appPathsIn:[appPathsIn]) addPath(appPath); }
 
 const addPath = additionalPath => {
+	const appPaths = session.get(APP_PATHS_SESSION_KEY)||[];
 	if (appPaths.includes(additionalPath)) return; 
 	appPaths.push(additionalPath); i18nCached = {};	/* need to recache */
+	session.set(APP_PATHS_SESSION_KEY, appPaths);
 }
 
 const getRendered = async (key, data, language=getSessionLang(), refresh=false) => (
@@ -26,7 +30,8 @@ async function get(key, language=getSessionLang(), refresh=false) {
 }
 
 async function getI18NObject(language=getSessionLang(), refresh=false) {
-	if (!i18nCached[language] || refresh) {
+	const appPaths = session.get(APP_PATHS_SESSION_KEY);
+	if ((!i18nCached[language]) || refresh) {
 		i18nCached[language] = {i18n:{}};
 		for (const appPath of appPaths) {
 			const i18nThisAppPath = `${appPath}/i18n/i18n_${language}.mjs`;
@@ -36,7 +41,7 @@ async function getI18NObject(language=getSessionLang(), refresh=false) {
 			} catch (err) {LOG.error(`Error importing i18n bundle for apppath ${appPath}. Error is ${err}.`);}
 		}
 	} 
-	
+
 	return i18nCached[language].i18n;
 }
 
