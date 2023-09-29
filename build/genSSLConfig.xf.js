5f718a597667b4b482f74d4a57531dc8d33bdb92
TekMonksGitHub <rvkapoor@tekmonks.com>
2023-09-29 15:54:05 +0900

    Fixes to genSSLConfig for build.

--- build/genSSLConfig.xf.js
+++ build/genSSLConfig.xf.js
@@ -11,8 +11,7 @@ const BACKEND_HOST_FILE = path.resolve(`${MONKSHU_PATH}/backend/server/conf/host
 const BLACKBOARD_CONF_FILE = path.resolve(`${MONKSHU_PATH}/backend/server/conf/blackboard.json`);
 const LOCAL_IP = Object.values(require("os").networkInterfaces()).reduce((r, list) => r.concat(list.reduce((rr, i) => 
     rr.concat(i.family=="IPv4" && !i.internal && i.address || []), [])), []);
-const DEFAULT_APP_NAME = fs.existsSync(`${MONKSHU_PATH}/frontend/apps`) ? 
-    fs.readdirSync(`${MONKSHU_PATH}/frontend/apps`)[0] : null;
+const DEFAULT_APP_NAME = _resolveDefaultAppNameSync();
  
 // build
 exports.make = async function(etcdir, open_ssl_conf, appname) {
@@ -28,17 +27,23 @@ exports.make = async function(etcdir, open_ssl_conf, appname) {
             false, {OPENSSL_CONF: open_ssl_conf});
 
         CONSTANTS.LOGINFO("Setting hostnames");
+        CONSTANTS.LOGINFO(`Resolved app name as ${appname||DEFAULT_APP_NAME}`);
         const frontend_host_file = path.resolve(`${MONKSHU_PATH}/frontend/apps/${appname||DEFAULT_APP_NAME}/conf/hostname.json`);
+        
         CONSTANTS.LOGINFO("Writing hostname to "+BACKEND_HOST_FILE);
-        await fspromises.writeFile(BACKEND_HOST_FILE, `"${LOCAL_IP}"`);
+        await _makePathWritable(BACKEND_HOST_FILE); await fspromises.writeFile(BACKEND_HOST_FILE, `"${LOCAL_IP}"`);
         CONSTANTS.LOGINFO("Writing hostname to "+frontend_host_file);
-        await fspromises.writeFile(frontend_host_file, `"${LOCAL_IP}"`);
+        await _makePathWritable(frontend_host_file); await fspromises.writeFile(frontend_host_file, `"${LOCAL_IP}"`);
+        
         CONSTANTS.LOGINFO("Writing blackboard config to "+BLACKBOARD_CONF_FILE);
         const blackboard_conf = (await fspromises.readFile(BLACKBOARD_CONF_FILE, "utf8")).replace(/\"secure\"\s*:\s*false/, `"secure": true`);
-        await fspromises.writeFile(BLACKBOARD_CONF_FILE, blackboard_conf);
-        await _modifyHTTPDConf(BACKEND_HTTPD_FILE, etcdir, false);
+        await _makePathWritable(BLACKBOARD_CONF_FILE); await fspromises.writeFile(BLACKBOARD_CONF_FILE, blackboard_conf);
+        
+        CONSTANTS.LOGINFO("Adding SSL certificate paths to "+BACKEND_HTTPD_FILE);
+        await _makePathWritable(BACKEND_HTTPD_FILE); await _modifyHTTPDConf(BACKEND_HTTPD_FILE, etcdir, false);
         const frontend_httpd_file = path.resolve(`${MONKSHU_PATH}/frontend/apps/${appname||DEFAULT_APP_NAME}/conf/httpd.json`);
-        await _modifyHTTPDConf(frontend_httpd_file, etcdir, true);
+        CONSTANTS.LOGINFO("Adding SSL certificate paths to "+frontend_httpd_file);
+        await _makePathWritable(frontend_httpd_file); await _modifyHTTPDConf(frontend_httpd_file, etcdir, true);
 
         CONSTANTS.LOGSUCCESS();
     } catch (err) { 
@@ -50,8 +55,24 @@ exports.make = async function(etcdir, open_ssl_conf, appname) {
 
 async function _modifyHTTPDConf(file, etcdir, setPort443) {
     CONSTANTS.LOGINFO("Writing httpd SSL config to the file "+file);
-    let httpd_conf; try {httpd_conf = JSON.parse(await fspromises.readFile(file, "utf8"));} catch (err) {}; httpd_conf = httpd_conf||{};
+    
+    let httpd_conf; try {httpd_conf = JSON.parse(await fspromises.readFile(file, "utf8"));} catch (err) {httpd_conf = {}};
+    
     httpd_conf.ssl = true; httpd_conf.sslKeyFile = path.resolve(`${etcdir}/dnsip_privkey.pem`);
     httpd_conf.sslCertFile = path.resolve(`${etcdir}/dnsip_fullchain.pem`); if (setPort443) httpd_conf.port = 443;
+    CONSTANTS.LOGINFO("Checking path exists "+path.dirname(file));
     await fspromises.writeFile(file, JSON.stringify(httpd_conf, null, 4));
+}
+
+async function _makePathWritable(file) {
+    if (!fs.existsSync(path.dirname(file))) fs.mkdirSync(path.dirname(file), {recursive: true}); 
+}
+
+function _resolveDefaultAppNameSync() {
+    if (!fs.existsSync(`${MONKSHU_PATH}/frontend/apps`)) return null;
+    
+    for (const direntry of fs.readdirSync(`${MONKSHU_PATH}/frontend/apps`)) 
+        if (fs.statSync(`${MONKSHU_PATH}/frontend/apps/${direntry}`).isDirectory()) return direntry;
+
+    return null;
 }
