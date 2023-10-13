dbed49ea2d10ddb2b6975435637b13d322ee31c1
TekMonksGitHub <rvkapoor@tekmonks.com>
2023-10-13 19:51:20 +0900

    Better process args parser.

--- backend/server/lib/processargs.js
+++ backend/server/lib/processargs.js
@@ -13,19 +13,36 @@
 let args;
 
 /**
+ * @param {Object} propMap The map of properties to expect, named and required.
  * @returns The process args as a key-value pair object. All values are arrays.
  */
-function getArgs() {
+function getArgs(propMap) {
     if (args) return args; else args = {};
+
+    const _getArgsKey = argOption => 
+        propMap && propMap[argOption] ? propMap[argOption].long||propMap[argOption] : argOption;
 
     const argv = process.argv.slice(2);
     let currKey; for (const arg of argv) {
-        if (arg.startsWith("-")||(process.platform=="win32" && arg.startsWith("/"))) {
-            currKey = arg.substring(1); args[currKey] = args[currKey]||[];
+        if (arg.startsWith("-")||(process.platform=="win32" && arg.startsWith("/"))||(arg.startsWith("--"))) {
+            currKey = _getArgsKey(arg.substring(arg.startsWith("--")?2:1)); args[currKey] = args[currKey]||[];
         } else if (currKey) args[currKey].push(arg);
     }
 
-    return args;
+    const errorKeys = [];
+    if (propMap) for (const key of Object.keys(propMap)) if (propMap[key].required && args[_getArgsKey(key)] && 
+        (!args[_getArgsKey(key)].length)) errorKeys.push(key);
+
+    if (errorKeys.length) {
+        console.error(propMap.description||"");
+        for (const errorKey of errorKeys) console.error(`${errorKey}: ${propMap[errorKey].help||`Missing value for ${errorKey}.`}`);
+        return null;
+    } else return args;
 }
 
-module.exports = {getArgs};
+module.exports = {getArgs, reset: _=>args=undefined};
+
+if (require.main === module) {
+    console.log(getArgs()); module.exports.reset();
+    console.log(getArgs({"c": "color", "f": {long: "file", required: true, help: "Filepath. The value is required if specified."}})||"Error in the input arguments.");
+}
