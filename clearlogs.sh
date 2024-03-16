9eea570026f611cdd5f8296b9a7ace1df602b77f
TekmonksGitHub <rvkapoor@tekmonks.com>
2024-03-17 03:30:42 +0800

    Fix clearlogs script on *nix platforms.

--- clearlogs.sh
+++ clearlogs.sh
@@ -4,5 +4,5 @@ CURPATH="$( cd "$( dirname "$0" )" && pwd )"
 rm "$CURPATH/frontend/server/logs/access.log.json"
 rm "$CURPATH/frontend/server/logs/error.log.json"
 
-rm "%CURPATH%/backend/server/logs/server.log.ndjson"
-rm "%CURPATH%/backend/server/logs/globalmemory.log.ndjson"
+rm "$CURPATH/backend/server/logs/server.log.ndjson"
+rm "$CURPATH/backend/server/logs/globalmemory.log.ndjson"
