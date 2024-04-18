6b0dad523bc2bccc5f51a8be2ce37ac95fca3390
TekmonksGitHub <rvkapoor@tekmonks.com>
2024-04-19 03:55:06 +0530

    Add message delivery options to blackboard.

--- backend/server/lib/blackboard.js
+++ backend/server/lib/blackboard.js
@@ -41,12 +41,13 @@ async function doService(request) {
 
 async function publish(topic, payload) {
     const poster = conf.secure?rest.postHttps:rest.post;
+    const _createBroadcastMessage = _ => {return {topic, payload, processid: process.pid}};
 
     let failedReplicas = 0;
     for (const replica of conf.replicas) {
         const host = replica.substring(0, replica.lastIndexOf(":")); const port = replica.substring(replica.lastIndexOf(":")+1);
         try {
-            await poster(host, port, API_BB_PATH, {}, {type:BLACKBOARD_MSG, msg:{topic, payload}}, (err,result) => {
+            await poster(host, port, API_BB_PATH, {}, {type:BLACKBOARD_MSG, msg:_createBroadcastMessage()}, (err,result) => {
                 if (err || !result.result) LOG.error(`Blackboard replication failed for replica: ${replica}`);
             });
         } catch (err) { LOG.error(`Blackboard can't reach replica ${replica}, error is ${err}.`); failedReplicas++; }
@@ -54,8 +55,8 @@ async function publish(topic, payload) {
 
     if ((!conf.replicas.length) || (failedReplicas == conf.replicas.length)) {   // if network is down or if all replicas are misconfigured, then at least broadcast to the local node so that applications relying on the blackboard keep running
         LOG.error(`Failed to reach all replicas. Assuming network isolated topology. Broadcasting the message locally.`);
-        if (process.send) process.send({type: BLACKBOARD_MSG, msg:{topic, payload}});   // broadcast to the local cluster if no replicas working
-        else _broadcast({topic, payload});  // if no local cluster then just give up and send to the local applications
+        if (process.send) process.send({type: BLACKBOARD_MSG, msg:_createBroadcastMessage()});   // broadcast to the local cluster if no replicas working
+        else _broadcast(_createBroadcastMessage());  // if no local cluster then just give up and send to the local applications
     }
 }
 
@@ -67,14 +68,20 @@ async function isEntireReplicaClusterOnline() {
     return true;
 }
 
-function subscribe(topic, callback) {
+function subscribe(topic, callback, options) {
     if (!topics[topic]) topics[topic] = [];
-    topics[topic].push(callback);
+    topics[topic].push({callback, options});
 }
 
 function _broadcast(msg) {
     const topic = msg.topic;
-    if (topics[topic]) for (const subscriber of topics[topic]) subscriber(msg.payload);
+    if (topics[topic]) for (const subscriber of topics[topic]) {
+        const {callback, options} = subscriber;
+        if (!options) callback(msg.payload);    // no options means receive all the time
+        else if (options[exports.LOCAL_ONLY] && (msg.processid == process.pid)) callback(msg.payload);
+        else if (options[exports.EXTERNAL_ONLY] && (msg.processid != process.pid)) callback(msg.payload);
+        else callback(msg.payload); // no valid option - so send it out still
+    }
 }
 
 function _expandConf(conf) {
@@ -82,4 +89,5 @@ function _expandConf(conf) {
     return retConf;
 }
 
-module.exports = {init, doService, publish, subscribe, isEntireReplicaClusterOnline}
+module.exports = {init, doService, publish, subscribe, isEntireReplicaClusterOnline, LOCAL_ONLY: "localonly", 
+    EXTERNAL_ONLY: "externalonly"};
