ff35ba7934373481b24f0169efa4fa7af9bed57d
TekmonksGitHub <rvkapoor@tekmonks.com>
2025-05-05 16:53:30 +0800

    Add in redirect base64 functionality.

--- frontend/server/ext/redirect.js
+++ frontend/server/ext/redirect.js
@@ -10,22 +10,26 @@ const utils = require(`${conf.libdir}/utils.js`);
 
 exports.name = "redirect";
 exports.processRequest = async (req, res, dataSender, _errorSender, _codeSender, access, _error) => {
-	const protocol = req.connection.encrypted ? "https" : "http",
-        {redirectedURL, code} = _getRedirectedURL(new URL(req.url, `${protocol}://${utils.getServerHost(req)}/`))||{redirectedURL:null, code:null}; 
+	const protocol = req.connection.encrypted ? "https" : "http";
+    const {redirectedURL, code} = _getRedirectedURL(new URL(req.url, `${protocol}://${utils.getServerHost(req)}/`)); 
 	if (!redirectedURL) return false;
 
 	access.info(`Redirecting, ${code}, ${req.url} to ${redirectedURL.href}`);
 	
-	dataSender(req, res, {"Location": redirectedURL.href}, null, null, null, 302); return true;
+	dataSender(req, res, {"Location": redirectedURL.href}, null, null, null, code); return true;
 }
 
 function _getRedirectedURL(url) {
-    if (!conf.redirects) return null; // no redirects configured
+    const nullReturn = {redirectedURL:null, code:null};
+    if (!conf.redirects) return nullReturn; // no redirects configured
+
     for (const redirect of conf.redirects) {
-        const match = url.href.match(new RegExp(Object.keys(redirect)[0])); if (match) {
+        const match = url.href.match(new RegExp(Object.keys(redirect)[0])); 
+        if (match) {
             const data = {}; for (let i = 1; i < match.length; i++) data[`$${i}`] = match[i];
+            data.base64 = _ => (text, render) => btoa(render(text));  // add base64 encoding function, useful for Monkshu framework
             return {redirectedURL: new URL(mustache.render(redirect[Object.keys(redirect)[0]], data)), code: 302}
         }
     }
-    return {redirectedURL: null, code: null};    // nothing matched
+    return nullReturn;    // nothing matched
 }
