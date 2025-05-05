541d9ff53000d9a5aeeeb3fe645ca7881949d377
TekmonksGitHub <rvkapoor@tekmonks.com>
2025-05-05 15:02:45 +0800

    Add preinit phase for apps - happens before networking etc are up.

--- backend/server/lib/app.js
+++ backend/server/lib/app.js
@@ -1,5 +1,5 @@
 /** 
- * (C) 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024. 
+ * (C) 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025 
  * TekMonks. All rights reserved.
  * License: See the enclosed LICENSE file.
  * 
@@ -10,20 +10,23 @@ const fs = require("fs");
 const appRoots = [];
 
 function initSync() {
-    for (const approotdir of (CONSTANTS.APPROOTDIRS||[CONSTANTS.APPROOTDIR])) if (fs.existsSync(approotdir)) 
-            for (const app of fs.readdirSync(approotdir)) {
+    for (const approotdir of (CONSTANTS.APPROOTDIRS||[CONSTANTS.APPROOTDIR])) 
+            if (fs.existsSync(approotdir)) for (const app of fs.readdirSync(approotdir)) {
         
-                appThis = {}; appThis[app] = `${approotdir}/${app}`; 
+        appThis = {}; appThis[app] = `${approotdir}/${app}`; 
         appRoots.push(appThis);
     }
+
+    initAppsSync(true); // pre-init happens before transport etc are up, init happens later
 }
 
-function initAppsSync() {
+function initAppsSync(preInit=false) {
     for (const appEntry of appRoots) {
         const app = Object.keys(appEntry)[0];
         if (fs.existsSync(`${appEntry[app]}/lib/app.js`)) {
-            LOG.info(`Initializing app: ${app}`);
-            const appThis = require(`${appEntry[app]}/lib/app.js`); if (appThis.initSync) appThis.initSync(app); 
+            LOG.info(`${preInit?"Preinitializing":"Initializing"} app: ${app}`);
+            const appThis = require(`${appEntry[app]}/lib/app.js`); 
+            if (preInit?appThis.preinitSync:appThis.initSync) preInit?appThis.preinitSync(app):appThis.initSync(app); 
         }
     }
 }
