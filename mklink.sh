b3823de674eabc81e848a8ffe2bd3bf488598243
himanshuDLTech <himanshu.gupta@deeplogictech.com>
2026-07-24 07:42:58 +0000

    [fix] updated mklink & rmlink scripts to support multiple symblinks

--- mklink.sh
+++ mklink.sh
@@ -1,18 +1,39 @@
 #!/bin/bash
 
 CURPATH="$( cd "$( dirname "$0" )" && pwd )"
-pushd . > /dev/null
-cd "$CURPATH/../"
+
+pushd "$CURPATH/../" > /dev/null
 APP_DIR=$PWD
 popd > /dev/null
+
 if [ "$1" == "" ]; then
-	echo Usage: $0 [name of the application]
-	exit 1
+    echo Usage: $0 [name of the application]
+    exit 1
 fi
+
 APP_NAME=$1
 
-ln -s "$APP_DIR/$APP_NAME/backend/apps" "$CURPATH/backend/apps" 
-ln -s "$APP_DIR/$APP_NAME/frontend/apps" "$CURPATH/frontend/apps"
-ln -s "$APP_DIR/$APP_NAME/desktop/app" "$CURPATH/desktop/app"
+for TYPE in backend frontend; do
+    SRC="$APP_DIR/$APP_NAME/$TYPE/apps"
+
+    if [ -d "$SRC" ]; then
+        mkdir -p "$CURPATH/$TYPE/apps"
+
+        for APP in "$SRC"/*; do
+            [ -d "$APP" ] || continue
+            ln -sfn "$APP" "$CURPATH/$TYPE/apps/$(basename "$APP")"
+        done
+    fi
+done
+
+SRC="$APP_DIR/$APP_NAME/desktop/app"
+if [ -d "$SRC" ]; then
+    mkdir -p "$CURPATH/desktop/app"
+
+    for APP in "$SRC"/*; do
+        [ -d "$APP" ] || continue
+        ln -sfn "$APP" "$CURPATH/desktop/app/$(basename "$APP")"
+    done
+fi
 
 echo Done.
